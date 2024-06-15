c78dde501c10a9718663d42476e5cfb9f7241993
Gauri Rajendra Hole <gaurihole2502@gmail.com>
2024-06-15 22:13:11 +0530

    Implementation of Input Class.

---
+++ InputEx.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class InputEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Full Name :\n ");
+        String name =sc.nextLine();
+        System.out.println(name);
+        System.out.println("Enter Name : ");
+        name =sc.next();
+        System.out.println(name);
+    }
+}
